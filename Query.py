12826d9ddf18999b2b57bf73a97a1c5bbac484d1
Jonathan DiMuccio <50346472+dimucciojonathan@users.noreply.github.com>
2019-11-21 00:50:46 -0500

    Add files via upload

--- Query.py
+++ Query.py
@@ -1,37 +1,28 @@
-import urllib.request
+import requests
 import json
-import csv
-import pandas as pd
 
 # New function that saves data to a CSV
 def player_info(memberTag):
-    # Keeps authentication key open for each transaction
-    token = 'clashkey.txt'
-    request = urllib.request.Request(
-        url,
-        None,
-        {
-            "Authorization": "Bearer %s" % token
-        }
-    )
-    member_response = urllib.request.urlopen(request).read().decode("utf-8")
-    member_data = json.loads(member_response)
-    return member_data
+    url = "https://api.clashroyale.com/v1/players/%23" + str(memberTag)
 
-def battle_log(memberTag):
-    token = 'clashkey.txt'
-    url = "https://api.clashroyale.com/v1/players/%23" + str(memberTag) + '/battlelog'
+    headers = {"Accept": "application/json",
+               "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImZlNzUyMmMyLTZiZjMtNGUyYS04YmUwLWM0YzdkMWQ1MjM0ZiIsImlhdCI6MTU3NDIxNjI5MCwic3ViIjoiZGV2ZWxvcGVyLzVkYmE4NzhjLWZiOWUtYTVlYy1lNGUyLWVkN2U3NjQ5NDcyOCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1MC44OC4yNDIuMTE3Il0sInR5cGUiOiJjbGllbnQifV19.2jMHppUrsLN9o2AfIyk1JBe96iIrcI2YhKUHrWI3RsrD_H4npoXPyHypTjZEXuPIGTaeaIGT6lcTewjUJkXwBA"
+               }
 
-    request = urllib.request.Request(
-        url,
-        None,
-        {
-            "Authorization": "Bearer %s" % token
-        }
-    )
-    member_response = urllib.request.urlopen(request).read().decode("utf-8")
-    member_data = json.loads(member_response)
-    return member_data
+    response = requests.request("GET", url, headers=headers)
+    #print(response.text)
+    data = response.json()
+    tag = data['tag']
+    username = data['name']
+    expLevel =  data['expLevel']
+    trophies = data['trophies']
+    wins = data['wins']
+    losses = data['losses']
+    bestTrophies = data['bestTrophies']
+    
+    return tag, username, expLevel, trophies, wins, losses, bestTrophies
 
 
-print(battle_log('YYRVLLUV'))
+tag, user, expLevel,trophies, wins,losses, best = player_info('YYRVLLUV')
+
+
--- db.py
+++ db.py
@@ -1,9 +1,33 @@
-import mysql.connector
+import sqlite3
+import Query
 
-mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='password'
-)
 
-print(mydb)
+# My tag is 'YYRVLLUV'
+def insert_data(tag):
+    tag, user, expLevel, trophies, wins, losses, best = Query.player_info(tag)
+
+    conn = sqlite3.connect("cr.db")
+    c = conn.cursor()
+
+    c.execute("INSERT INTO profile VALUES (:tag, :name, :expLevel, :trophies, :wins, :losses, :bestTrophies)",
+          {'tag': tag, 'name': user, 'expLevel': expLevel, 'trophies': trophies, 'wins': wins, 'losses': losses, 'bestTrophies': best})
+
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
+# c.execute("""CREATE TABLE profile (
+#            tag text,
+#            name text,
+#            expLevel text,
+#            trophies integer,
+#            wins integer,
+#            losses integer,
+#            bestTrophies integer
+#    )""")
+
+
